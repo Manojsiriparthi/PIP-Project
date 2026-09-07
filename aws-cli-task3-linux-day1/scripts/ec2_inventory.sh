c57e7c115143e0a5663514ea7c14e7205b6d50f7
manojsiriparthi <manojs.devops1@gmail.com>
2026-09-07 12:37:51 +0000

    Deleted duplicate files

--- aws-cli-task3-linux-day1/scripts/ec2_inventory.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-REGION="${1:-ap-south-1}"
-
-aws ec2 describe-instances   --region "$REGION"   --query 'Reservations[].Instances[].{InstanceId:InstanceId,Type:InstanceType,State:State.Name,AZ:Placement.AvailabilityZone,PrivateIP:PrivateIpAddress,PublicIP:PublicIpAddress}'   --output table
